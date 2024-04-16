575e252df57c4f0ca8e907a19bafd596861f58b1
eishah-iqbal <165206605+eishah-iqbal@users.noreply.github.com>
2024-04-16 23:16:30 +0500

    Update User.h

--- User.h
+++ User.h
@@ -1,18 +1,18 @@
-//#define User.h
-//#include <iostream>
-//using namespace std;
-//class user
-//{
-//	int mobile_num;
-//	string email;
-//	string password;
-//	string address;
-//public:
-//	string name;
-//
-//	user() {};
-//	void registration();
-//	void login();
-//	~user() {};
-//
-//};
+#define User.h
+#include <iostream>
+using namespace std;
+class user
+{
+	int mobile_num;
+	string email;
+	string password;
+	string address;
+public:
+	string name;
+
+	user() {};
+	void registration();
+	void login();
+	~user() {};
+
+};
